0395c4c085b4428420f41a803c426f0efb2f2a53
anyahush <anyahush@gmail.com>
2021-11-13 10:51:38 +0000

    Creat sendMail function to allow enquiries to be sent ot site owner

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,1 +1,17 @@
-console.log("hello");
+function sendMail(contactForm) {
+    emailjs.send("service_a66lyia", "quiz", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "subject": contactForm.subject.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("ERROR", error);
+        }
+    );
+    return false;
+}
