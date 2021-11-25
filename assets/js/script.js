248bd250ba0cbfe1c9c80c36fd407ccb5999b078
anyahush <anyahush@gmail.com>
2021-11-25 10:49:52 +0000

    Make changes from W3 HTML validator

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var spans = document.getElementsByClassName("close");
 for (var i = 0; i < btn.length; i++) {
   btn[i].onclick = function (e) {
     e.preventDefault();
-    modal = document.querySelector(e.target.getAttribute("href"));
+    modal = document.querySelector(e.target.getAttribute("data-target"));
     modal.style.display = "block";
   }
 }
