0f8ce7c3e2534c1c72c4cf31d2a535b2c0d04d91
anyahush <anyahush@gmail.com>
2021-12-01 10:14:24 +0000

    Remove closeModal function and create sendForm function

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,33 +1,37 @@
+// // End of W3 schools 
 
 
-// Contact form
-// Modified from W3 schools tutorial, full details in README.md
-function closeModal() {
-    var myModal2 = document.getElementById("myModal2");
-    if (myModal2.style.display === "none") {
-        myModal2.style.display = "block";
-    } else {
-        myModal2.style.display = "none";
-        alert("Thank you for your enquiry. We will respond as soon as possible.");
+const contactForm = document.getElementById("contact-form");
+const myModal2 = document.getElementById("myModal2");
+const sendButton = document.getElementById("send-btn");
+
+contactForm.addEventListener('submit', sendForm);
+sendButton.addEventListener("click", () => {
+    myModal2.style.display = "none";
+    alert("Thank you for your enquiry. We will respond as soon as possible.");
+})
+
+function sendForm(event) {
+    event.preventDefault();
+
+    return sendMail(this);
+
+    function sendMail(contactForm) {
+        emailjs.send("service_a66lyia", "quiz", {
+                "from_name": contactForm.name.value,
+                "from_email": contactForm.email.value,
+                "message": contactForm.message.value
+            })
+            .then(
+                function (response) {
+                    console.log("SUCCESS", response);
+                },
+                function (error) {
+                    console.log("ERROR", error);
+                }
+            );
     }
+
 }
-// End of W3 schools 
 
 
-
-function sendMail(contactForm) {
-    emailjs.send("service_a66lyia", "quiz", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("ERROR", error);
-        }
-    );
-    return false;
-}
