00a1c6a14e7dd9c789affe647221a4f94b510dbb
anyahush <anyahush@gmail.com>
2021-12-01 13:46:31 +0000

    Add functions to validate user input into contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,103 @@
-// // End of W3 schools 
-
 
 const contactForm = document.getElementById("contact-form");
 const myModal2 = document.getElementById("myModal2");
 const sendButton = document.getElementById("send-btn");
+
+// code modified from JavaScript Tutorial, full details in README
+const usernameEl = document.getElementById('name');
+const emailEl = document.getElementById('email');
+const messageEl = document.getElementById('message');
+
+const checkUsername = () => {
+
+    let valid = false;
+
+    let min = 3,
+        max = 25;
+
+    const username = usernameEl.value.trim();
+
+    if (!isRequired(username)) {
+        showError(usernameEl, 'Username cannot be blank.');
+    } else if (!isBetween(username.length, min, max)) {
+        showError(usernameEl, `Username must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(usernameEl);
+        valid = true;
+    }
+    return valid;
+};
+
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+};
+
+const checkMessage = () => {
+    let valid = false;
+    let min = 10
+
+    const message = messageEl.value.trim();
+
+    if (!isRequired(message)) {
+        showError(messageEl, 'Message cannot be blank.');
+    } else if (!isBetween(message.length, min, max)) {
+        showError(usernameEl, `Message must be minimum ${min} characters.`)
+        showSuccess(messageEl);
+        valid = true;
+    }
+
+    return valid;
+};
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const isNameValid = (username) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])");
+    return re.test(username);
+};
+
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
 
 contactForm.addEventListener('submit', sendForm);
 sendButton.addEventListener("click", () => {
@@ -14,6 +108,21 @@ sendButton.addEventListener("click", () => {
 function sendForm(event) {
     event.preventDefault();
 
+    // validate fields
+    let isUsernameValid = checkUsername(),
+        isEmailValid = checkEmail(),
+        isPasswordValid = checkPassword(),
+        isConfirmPasswordValid = checkConfirmPassword();
+
+    let isFormValid = isUsernameValid &&
+        isEmailValid &&
+        isPasswordValid &&
+        isConfirmPasswordValid;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+
+    }
     return sendMail(this);
 
     function sendMail(contactForm) {
