cf01dfe2fc1026abd78aa3334895404ddf321b0e
anyahush <anyahush@gmail.com>
2021-12-01 09:21:03 +0000

    Move closeModal function and rename sendemail.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,20 @@
+
+
+// Contact form
+// Modified from W3 schools tutorial, full details in README.md
+function closeModal() {
+    var myModal2 = document.getElementById("myModal2");
+    if (myModal2.style.display === "none") {
+        myModal2.style.display = "block";
+    } else {
+        myModal2.style.display = "none";
+        alert("Thank you for your enquiry. We will respond as soon as possible.");
+    }
+}
+// End of W3 schools 
+
+
+
 function sendMail(contactForm) {
     emailjs.send("service_a66lyia", "quiz", {
         "from_name": contactForm.name.value,
