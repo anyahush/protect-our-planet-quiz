9032790333289cade9f786104fa308c974a419f3
anyahush <anyahush@gmail.com>
2021-12-02 10:37:52 +0000

    Fix variable name in validateForm

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,25 +9,24 @@ contactForm.addEventListener('submit', function(e) {
 });
 
 function validateForm(contactForm) {
-    let name = document.getElementById("name").value;
-    let emailAddress = document.getElementById("emailaddress").value;
+    let nameInput = document.getElementById("name").value;
+    let emailInput = document.getElementById("emailaddress").value;
     let message = document.getElementById("message").value;
     let errorMessage = document.getElementsByClassName("error-message");
 
   
-    if(name.length < 5){
-        errorMessage.innerHTML = "Please enter valid name";
+    if(nameInput.length < 5){
+        errorMessage[0].innerHTML = "Please enter valid name";
         console.log(errorMessage);
         return false;
     }
-    if(emailAddress.indexOf("@") == -1) {
-        errorMessage.innerHTML =  "Please enter valid email address";
+    if(emailInput.indexOf("@") == -1) {
+        errorMessage[1].innerHTML =  "Please enter valid email address";
         console.log(errorMessage);
         return false;
     }
-    if(message.length <= 100) {
-        errorMessage.innerHTML = "Please enter more than 100 characters";
-        console.log(errorMessage);
+    if(message.length <= 50) {
+        errorMessage[2].innerHTML = "Please enter more than 50 characters";
         return false;
     }
 
@@ -38,7 +37,7 @@ function validateForm(contactForm) {
     function sendMail(contactForm) {
         emailjs.send("service_a66lyia", "quiz", {
                 "from_name": contactForm.name.value,
-                "from_email": contactForm.email.value,
+                "from_email": contactForm.emailaddress.value,
                 "message": contactForm.message.value
             })
             .then(
